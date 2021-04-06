da36c27041a348f89c076eb1f1862d3051b927bc
Josh Stephenson <joshstephenson55@gmail.com>
2021-04-06 18:24:14 +1000

    added basic menu methods for inventory and loyalty program.

--- main.rb
+++ main.rb
@@ -1,52 +1,88 @@
 require 'csv'
+
 inventory = []
 loyalty = []
 prices = {small: 2, medium: 3, large: 4}
 sales = {small: 0, medium: 0, large: 0}
 
-def coffeeOrder
-            puts "\e[H\e[2J"
-            puts "What type of coffee?"
-            type = gets.chomp.downcase
-            puts "What size?"
-            size = gets.chomp.downcase
-            puts "Milk?"
-            milk = gets.chomp.downcase
-            puts "Sugar?"
-            sugar = gets.chomp.downcase
-            puts "Enter loyalty card number if available"
-            loyalty = gets.chomp.to_i
-            puts "#{size} #{type} with #{milk} and #{sugar}"
-            puts "Is this order correct?"
-            response = gets.chomp.downcase
+def coffeeOrder(inventory, loyalty)
+    puts "\e[H\e[2J"
+    puts "What type of coffee?"
+    type = gets.chomp.downcase
+    puts "What size?"
+    size = gets.chomp.downcase
+    puts "Milk?"
+    milk = gets.chomp.downcase
+    puts "Sugar?"
+    sugar = gets.chomp.downcase
+    puts "Enter loyalty card number if available"
+    loyalty_num = gets.chomp.to_i
+    puts "#{size} #{type} with #{milk} and #{sugar}"
+    puts "Is this order correct?"
+        response = gets.chomp.downcase
             if response == "yes"
-                removeCup
-                mainMenu
+                removeCup(inventory)
+                checkLoyalty(loyalty, loyalty_num)
+                mainMenu(inventory, loyalty)
             else
                 coffeeOrder
             end
     
-        end
+end
 
-def removeCup
-    #Find cups in inventory
-    result = inventory.find {|item| "cups" == item[:name] }
+def removeCup(array)
+    #Find cups
+    result = array.find {|item| "cups" == item[:name] }
     #remove 1 cup
     result[:qty] = result[:qty] - 1
 end
 
-def checkLoyalty
-    result = loyalty.find do |number|
-        if  loyalty ==  number[:loyalty_number]
-            number[:qty] + 1
-             if number[:qty] == 5
+def checkLoyalty(array, loyalty)
+    result = array.each do |item|
+        if  loyalty ==  item[:loyalty_number]
+            result[:qty] + 1
+             if result[:qty] == 5
                 puts "Free 5th Coffee"
-                number[:qty] = 0
+                sleep 5
+                result[:qty] = 0
             end
-            
         end
+    end
 end
 
+def displayInventory(array, loyalty)
+    puts "\e[H\e[2J"
+    array.each do |line|
+        puts "You have #{line[:qty]} units of #{line[:name]}"
+    end
+    puts "--------------------------------------"
+    puts "         Options"
+    puts "1. Edit Inventory 2. Exit"
+    puts "--------------------------------------"
+    input = gets.chomp
+    if input == "1"
+        #Execute edit method
+    elsif input == "2"
+        mainMenu(array, loyalty)
+    end
+end
+
+def displayLoyalty(inventory,loyalty)
+    puts "\e[H\e[2J"
+    puts "Enter loyalty number"
+    result = gets.chomp.to_i
+    loyalty.each do |item|
+    end
+end
+
+def endofDay
+    #  write inventory array to inventory.csv
+    CSV.open('./csv/inventory.csv', "w") do |csv|
+         inventory.each do |item|
+        csv << [item[:name], item[:qty]]
+        end
+    end
+end
 
 #open inventory.csv and save to array
 CSV.open("./csv/inventory.csv") do |csv|
@@ -65,28 +101,26 @@ end
 
 puts "Welcome to CoffeePOS!"
 
-def mainMenu
+def mainMenu(inventory, loyalty)
     puts "\e[H\e[2J"
-    puts "-----------------------------------------------"
+    puts "-----------------------------------------------------------------"
     puts "1. New Order   2. Inventory  3. Loyalty Program   4. End of day"
     puts "Enter the number of the option you would like"
-    puts "-----------------------------------------------"
+    puts "-----------------------------------------------------------------"
     action = gets.chomp.downcase 
     if action == "1"
-        coffeeOrder
+        coffeeOrder(inventory, loyalty)
     elsif action == "2"
-        # inventory = Inventory.new
+        displayInventory(inventory,loyalty)
     elsif action == "3"
-        # loyalty = Loyalty.new
     elsif action == "4"
-        # end_of_day = Eod.new
     else
         puts "Please enter 1, 2, 3 or 4"
         mainMenu
     end
 end
 
-mainMenu
+mainMenu(inventory, loyalty)
 
 
 
@@ -102,5 +136,3 @@ mainMenu
 #     inventory.each do |item|
 #         csv << [item[:name], item[:qty]]
 #     end
-
-# end
